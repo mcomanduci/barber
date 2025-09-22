3a5be14287d89a26cbb2b0ce9b8c219e6dce3811
Marcelo <mcomanduci@gmail.com>
2025-09-22 19:13:13 -0300

    feat: add BookingItem component

---
+++ src/app/_components/booking-item.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+// TODO: Receber dados via props
+const BookingItem = () => {
+  return (
+    <>
+      <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
+        Agendamentos
+      </h2>
+
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit rounded-2xl">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+            <div className="item-center flex gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage
+                  src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+                  alt="Barbeiro"
+                />
+              </Avatar>
+              <p className="text-sm">Barbearia FSW</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Setembro</p>
+            <p className="text-2xl">22</p>
+            <p className="text-sm">15:00</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default BookingItem;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,12 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
-import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar, AvatarImage } from "./_components/ui/avatar";
 import BarbershopItem from "./_components/barbershop-item";
 import { db } from "./_lib/prisma";
 import { Barbershop } from "@prisma/client";
 import { Footer } from "./_components/footer";
+import { quickSearchOptions } from "./_constants/search";
+import BookingItem from "./_components/booking-item";
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({});
@@ -26,7 +25,6 @@ const Home = async () => {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Marcelo!</h2>
         <p>Segunda-feira, 22 de Setembro</p>
-
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -35,40 +33,21 @@ const Home = async () => {
         </div>
 
         <div className="mt-6 -mr-5 flex gap-3 overflow-x-scroll pr-5 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-          <Button className="mt-6 flex-1" variant="secondary">
-            <Image
-              src="/scissors.svg"
-              alt="Cabeleireiro"
-              width={24}
-              height={24}
-            />
-            Cabelo
-          </Button>
-          <Button className="mt-6 flex-1" variant="secondary">
-            <Image src="/mustache.svg" alt="Barba" width={16} height={16} />
-            Barba
-          </Button>
-          <Button className="mt-6 flex-1" variant="secondary">
-            <Image src="/razor.svg" alt="Acabamento" width={16} height={16} />
-            Acabamento
-          </Button>
-          <Button className="mt-6 flex-1" variant="secondary">
-            <Image
-              src="/eyebrow.svg"
-              alt="Sobrancelha"
-              width={16}
-              height={16}
-            />
-            Sobrancelha
-          </Button>
-          <Button className="mt-6 flex-1" variant="secondary">
-            <Image src="/towel.svg" alt="Massagem" width={16} height={16} />
-            Massagem
-          </Button>
-          <Button className="mt-6 flex-1" variant="secondary">
-            <Image src="/shampoo.svg" alt="Hidratação" width={16} height={16} />
-            Hidratação
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button
+              key={option.title}
+              className="mt-6 flex-1"
+              variant="secondary"
+            >
+              <Image
+                src={option.imageURL}
+                alt={option.title}
+                width={24}
+                height={24}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
@@ -80,32 +59,7 @@ const Home = async () => {
           />
         </div>
 
-        <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
-          Agendamentos
-        </h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit rounded-2xl">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="item-center flex gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage
-                    src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
-                    alt="Barbeiro"
-                  />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Setembro</p>
-              <p className="text-2xl">22</p>
-              <p className="text-sm">15:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
           Recomendados
