886bd296d4d823d502241c7361f76d7eeda6bade
Marcelo <mcomanduci@gmail.com>
2025-09-21 22:02:03 -0300

    chore: add prisma setup

---
+++ src/app/libs/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+// This helps prevent creating too many PrismaClient instances in development
+// due to Next.js's hot-reloading feature.
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Initialize the Prisma Client, extending it with Accelerate
+const prisma =
+  globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate());
+
+// Export the single instance
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
