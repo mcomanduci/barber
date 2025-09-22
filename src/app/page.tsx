11446e0790e966cea75197b9234674264fffdcd6
Marcelo <mcomanduci@gmail.com>
2025-09-22 12:20:24 -0300

    feat: add Header components and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-500">red text</h1>
-      <Button>Click me</Button>
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Marcelo!</h2>
+        <p>Segunda-feira, 22 de Setembro</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        <Card>
+          <CardContent></CardContent>
+        </Card>
+      </div>
     </>
   )
 }
