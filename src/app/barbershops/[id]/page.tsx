af5b4a8494a000fe81afa41ce58d98f80407ca79
Marcelo <mcomanduci@gmail.com>
2025-09-22 23:23:33 -0300

    feat: ensure barbershop services are shown on barbershop page

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ServiceItem from "@/app/_components/service-item";
 import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
@@ -12,6 +13,9 @@ interface BarbershopPageProps {
 const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const barbershop = await db.barbershop.findUnique({
     where: { id: params.id },
+    include: {
+      services: true,
+    },
   });
 
   if (!barbershop) {
@@ -49,11 +53,11 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       </div>
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
-        <div className="item-center mb-2 flex gap-1">
+        <div className="item-center mb-2 flex gap-2">
           <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop.address}</p>
         </div>
-        <div className="item-center flex gap-1">
+        <div className="item-center flex gap-2">
           <StarIcon className="text-primary fill-primary" size={18} />
           <p className="text-sm">5.0 (499 avaliações)</p>
         </div>
@@ -61,6 +65,14 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold text-gray-400 uppercase">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop.description}</p>
+      </div>
+      <div className="space-y-3 p-5">
+        <h2 className="text-xs font-bold text-gray-400 uppercase">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   );
